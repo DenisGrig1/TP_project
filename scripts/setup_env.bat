27499103732489c360cc004bad1ac289e9818a7a
DenisGrig1 <denisgrignew@gmail.com>
2026-02-25 09:18:12 +0400

    Update setup_env.bat

--- scripts/setup_env.bat
+++ scripts/setup_env.bat
@@ -1,6 +1,12 @@
 where python
+pause
 py -m venv .venv
+pause
 .\.venv\Scripts\Activate.ps1
+pause
 pip install -r requirements.txt
+pause
 python -c "import sem2_de; print('ok')"
+pause
 conda run -n broken_env.py
+pause
